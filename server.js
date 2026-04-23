022ed946da721382c5d2357e4cb133fc2f95c718
ajeetbijarniya6-dev <ajeetbijarniya6@gmail.com>
2026-04-23 10:58:55 +0530

    dfg

--- server.js
+++ server.js
@@ -16,6 +16,8 @@ app.listen(5000 , () => {
     console.log("server running on 5000");
 })
 
+
+
 // 2.  phele server chalAne ke liye => node server.js
 
 //  3.  on browser => localhost:5000
@@ -24,6 +26,58 @@ app.listen(5000 , () => {
 
 // go to package .json  => script =>  "start" : "nodemon server.js"
 
-
 /// finall => server chalane le liye => npm start 
+
+
+// app.get("/user" , (req, res) => {
+//     res.json({name : "suman" , age : "21"});
+// });
+
+
+app.use(express.json());
+
+// app.post("/mode" , (req , res)  => {
+//     res.json(req.body);
+// });
+
+
+let products = [];
+
+app.post("/product" , (req,res) => {
+    products.push(req.body);
+    res.json(products);
+});
+
+
+app.get("/product" , (req , res) => {
+    res.json(products);
+});
+
+
+
+app.post("/api" , (req , res) => {
+    console.log(req.body);
+
+    res.json({
+        message : "data received",
+        data : req.body
+    });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
